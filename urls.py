d01b89106541bd10b787a78aef6cdbf3fd527e54
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-07-13 01:48:26 -0700

    Mistake

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from .views import *
-urlpatterns = [
-    path('auth/register/',RegisterView.as_view()),
-    path('auth/',LoginView.as_view()),
-    path('connect/',CheckView.as_view())
-]
